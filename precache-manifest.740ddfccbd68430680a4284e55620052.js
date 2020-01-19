48d6ccb2e3ed76ec3344a2ed7f3241f30390b1ca
GretaVait <50132963+GretaVait@users.noreply.github.com>
2020-01-20 00:03:52 +0200

    Updates

---
+++ precache-manifest.740ddfccbd68430680a4284e55620052.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd394dee988d12939d269e763f77e72",
+    "url": "/shortly/index.html"
+  },
+  {
+    "revision": "5f408e180425f5fd2cd2",
+    "url": "/shortly/static/css/main.73678aa0.chunk.css"
+  },
+  {
+    "revision": "92a420a4cb92ec1e390e",
+    "url": "/shortly/static/js/2.0030c983.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shortly/static/js/2.0030c983.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f408e180425f5fd2cd2",
+    "url": "/shortly/static/js/main.c24d1506.chunk.js"
+  },
+  {
+    "revision": "937f34151746187c2962",
+    "url": "/shortly/static/js/runtime-main.7e79643f.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/shortly/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/shortly/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/shortly/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/shortly/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "e0029bcc17121d744694ad9c169717ad",
+    "url": "/shortly/static/media/close.e0029bcc.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/shortly/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/shortly/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/shortly/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/shortly/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/shortly/static/media/logo.6387840f.svg"
+  },
+  {
+    "revision": "04518f6caedbaf91050ce37c2d2ae82d",
+    "url": "/shortly/static/media/menu.04518f6c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shorty/precache-manifest.99335464389e852249de2b1d62c09e2f.js"
+  "/shortly/precache-manifest.740ddfccbd68430680a4284e55620052.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shorty/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shortly/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
