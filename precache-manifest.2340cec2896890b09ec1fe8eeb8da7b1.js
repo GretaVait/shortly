e8c773219febb6723312a8213412d2526c32f9d0
GretaVait <50132963+GretaVait@users.noreply.github.com>
2020-01-19 20:59:20 +0200

    Updates

---
+++ precache-manifest.2340cec2896890b09ec1fe8eeb8da7b1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d6e03ec4c82bd0a4095b5077cc5e73",
+    "url": "/GretaVait/shorty/index.html"
+  },
+  {
+    "revision": "a8f32a91701cfbe10ad5",
+    "url": "/GretaVait/shorty/static/css/main.da0eb313.chunk.css"
+  },
+  {
+    "revision": "92a420a4cb92ec1e390e",
+    "url": "/GretaVait/shorty/static/js/2.0030c983.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/GretaVait/shorty/static/js/2.0030c983.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8f32a91701cfbe10ad5",
+    "url": "/GretaVait/shorty/static/js/main.ff28faf6.chunk.js"
+  },
+  {
+    "revision": "0e20ed4ae6972f48d1d8",
+    "url": "/GretaVait/shorty/static/js/runtime-main.4024cd34.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/GretaVait/shorty/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/GretaVait/shorty/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/GretaVait/shorty/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/GretaVait/shorty/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "e0029bcc17121d744694ad9c169717ad",
+    "url": "/GretaVait/shorty/static/media/close.e0029bcc.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/GretaVait/shorty/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/GretaVait/shorty/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/GretaVait/shorty/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/GretaVait/shorty/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/GretaVait/shorty/static/media/logo.6387840f.svg"
+  },
+  {
+    "revision": "04518f6caedbaf91050ce37c2d2ae82d",
+    "url": "/GretaVait/shorty/static/media/menu.04518f6c.svg"
+  }
+]);
